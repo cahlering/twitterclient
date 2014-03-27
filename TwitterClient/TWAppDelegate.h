5362f735c6d8d12266c154f7fd403910515c11a3
cahlering <cahlering@richrelevance.com>
2014-03-26 20:10:34 -0700

    Initial Commit

---
+++ TwitterClient/TWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWAppDelegate.h
+//  TwitterClient
+//
+//  Created by Chris Ahlering on 3/26/14.
+//  Copyright (c) 2014 Chris Ahlering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TwitterClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwitterClient
+//
+//  Created by Chris Ahlering on 3/26/14.
+//  Copyright (c) 2014 Chris Ahlering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
